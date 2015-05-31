e28d6a1beeaecf34e615ccdbf4a6021f2322be35
Alex <deadsummer@yandex.ru>
2015-05-31 16:35:55 +0300

    4

--- url.py
+++ url.py
@@ -4,13 +4,17 @@ import urllib.request
 import codecs
 import text_analysis as txtan
 import numpy as np
+import scipy
 
-from sklearn.decomposition import TruncatedSVD
-from sklearn.random_projection import sparse_random_matrix
+
+import numpy as np
+import scipy.sparse as sp
+
 
 from bs4 import BeautifulSoup
 import re
-f = codecs.open('wiki.txt','w', encoding='utf-8')
+w = codecs.open('wiki.txt','w', encoding='utf-8')
+f = codecs.open('words.txt','w', encoding='utf-8')
 def GetURL(url):
     s = 'error'
     try:
@@ -23,21 +27,48 @@ def GetURL(url):
     return s
  
 
-soup = BeautifulSoup(GetURL('https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%BA%D0%BE%D0%BD_%D0%91%D0%B8%D0%BE_%E2%80%94_%D0%A1%D0%B0%D0%B2%D0%B0%D1%80%D0%B0_%E2%80%94_%D0%9B%D0%B0%D0%BF%D0%BB%D0%B0%D1%81%D0%B0'))
+start_page='https://ru.wikipedia.org/wiki/%D0%9F%D1%83%D1%88%D0%BA%D0%B8%D0%BD,_%D0%90%D0%BB%D0%B5%D0%BA%D1%81%D0%B0%D0%BD%D0%B4%D1%80_%D0%A1%D0%B5%D1%80%D0%B3%D0%B5%D0%B5%D0%B2%D0%B8%D1%87'
 
-for x in soup.find_all(class_="reference"):
-	x.extract()
-for x in soup.find_all(class_="mw-editsection"):
-	x.extract()
-for x in soup.find_all(style="display:none"):
-	x.extract()
-for x in soup.find_all(id="toc"):
-	x.extract()
-for x in soup.find_all(class_="infobox vcard"):
-	x.extract()
+def parser1 (link, text):
+	s = BeautifulSoup(GetURL(link))
+	content=s.find(id="mw-content-text")
+	for x in content.find_all(class_="reference"):
+		x.extract()
+	for x in content.find_all(class_="mw-editsection"):
+		x.extract()
+	for x in content.find_all(style="display:none"):
+		x.extract()
+	for x in content.find_all(id="toc"):
+		x.extract()
+	for x in content.find_all(class_="infobox vcard"):
+		x.extract()
+	text=text+"\n"+content.get_text()
+	return text
+	
+def parser2 (content,links):
+	a=content.findAll('a')
+	for x in a:
+		links.append("http://ru.wikipedia.org"+x['href'])
+	n=0
+	if input()=="g":
+		print(len(links))
+		n=n+1
+		s = BeautifulSoup(GetURL(links[n]))
+		content=s.find(id="mw-content-text")
+		links=parser2(content,links)
+	else:
+		return(links)
+
+soup = BeautifulSoup(GetURL(start_page))
 content=soup.find(id="mw-content-text")
-
-text=content.get_text()
+links=['https://ru.wikipedia.org/wiki/%D0%9B%D0%B5%D1%80%D0%BC%D0%BE%D0%BD%D1%82%D0%BE%D0%B2,_%D0%9C%D0%B8%D1%85%D0%B0%D0%B8%D0%BB_%D0%AE%D1%80%D1%8C%D0%B5%D0%B2%D0%B8%D1%87','https://ru.wikipedia.org/wiki/%D0%9F%D1%83%D1%88%D0%BA%D0%B8%D0%BD,_%D0%90%D0%BB%D0%B5%D0%BA%D1%81%D0%B0%D0%BD%D0%B4%D1%80_%D0%A1%D0%B5%D1%80%D0%B3%D0%B5%D0%B5%D0%B2%D0%B8%D1%87']
+text=""
+#links=parser2(content, links)
+#print(links)
+for x in links:
+	text=parser1(x,text)
+w.write(text)
+print("ready1///")
 sent=txtan.split_text(text)
 
 s_w=[]
@@ -50,7 +81,7 @@ roots1=[]
 for x in s_w_roots:
 	for k in x:
 		roots1.append(k)
-print("ready///")		
+print("ready2///")		
 roots=[]
 while len(roots1)>0:
 	roots.append(roots1[0])
@@ -63,7 +94,7 @@ while len(roots1)>0:
 	del roots1[0]
 	
 n_roots=len(roots)
-print("ready///")
+print("ready3///")
 
 r2i={}
 for i,x in enumerate(roots):
@@ -84,30 +115,24 @@ prop_r_r/=total_pairs
 print("ready///")
 for i in range(n_roots):
 	for j in range(n_roots):
-		if prop_r_r[i][j]>0.01:
+		if prop_r_r[i][j]>0.001:
 			f.write(roots[i])
 			f.write("\t")
 			f.write(roots[j])
 			f.write("\n")
 f.write("\n---------\n")
-def low_rank_approx(SVD=None, A=None, r=1):
-    """
-    Computes an r-rank approximation of a matrix
-    given the component u, s, and v of it's SVD
+prop_r_r=[[1,3,3,4,50],[2,4,-6,8,10],[10,10,30,-40,50],[1,40,3,4,5]]
+prop_r_r = np.array(prop_r_r,dtype=np.float)
 
-    Requires: numpy
-    """
-    if not SVD:
-        SVD = np.linalg.svd(A, full_matrices=False)
-    u, s, v = SVD
-    Ar = np.zeros((len(u), len(v)))
-    for i in range(r):
-        Ar += s[i] * np.outer(u.T[i], v[i])
-    return Ar
+def low_rank_approx(A=None, r=1):
+	from sklearn.decomposition import TruncatedSVD
+	svd  =  TruncatedSVD(n_components = r,  random_state = 42)
+	A=svd.fit_transform(A)
 
-M=prop_r_r-low_rank_approx(A=prop_r_r,r=20)
-			
-for i,j in zip(*np.where(M>=(0.002))):
+	return(np.dot(A,svd.components_))
+	
+M=prop_r_r-low_rank_approx(A=prop_r_r,r=1)		
+for i,j in zip(*np.where(M>=(M.max()/2))):
 	f.write(roots[i]+"  "+roots[j]+"\n")
 	
 	#bitch!
